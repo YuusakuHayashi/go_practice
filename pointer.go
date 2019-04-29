4cec59ba1c4c51ec2ce9b82064db4cfc693f5c58
yuusaku.hayashi <forestmtree@gmail.com>
2019-04-29 13:24:34 +0000

    test

--- pointer.go
+++ pointer.go
@@ -7,9 +7,9 @@ import (
 func main () {
 	var i0 int
 	var i1 *int
-	fmt.Println(i0)
-	fmt.Println(i1)
+	fmt.Println(i1)		// 初期値は<nil>
 	i1 = &i0 
-	fmt.Println(i1)
-	fmt.Println(*i1)
+	fmt.Println(i1)		//i0のアドレスを出力
+	fmt.Println(*i1)	//i0の値を参照
+	//i1 = i0 		//コンパイルエラー
 }
