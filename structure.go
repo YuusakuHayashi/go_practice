b423393562add73a5a370320bfa0d36ab02c2fbd
yuusaku.hayashi <forestmtree@gmail.com>
2019-04-30 15:14:42 +0000

    test

--- structure.go
+++ structure.go
@@ -4,10 +4,29 @@ import (
 	"fmt"
 )
 
+type hys_interface interface {
+	hys_method()
+}
+
 type Hys struct {
 	first string
 	last  string
 } 
+
+type Ysk struct {
+	//first string
+	//last  string
+} 
+
+func (h Hys) hys_method () {
+	//fmt.Printf("hello my name is %s %s \n",h.first, h.last)
+	fmt.Printf("hello\n")
+}
+
+func (y Ysk) hys_method () {
+	//fmt.Printf("hello my name is %s %s \n",y.first, y.last)
+	fmt.Printf("hello\n")
+}
 
 func main () {
 /*
@@ -25,10 +44,20 @@ func main () {
 	u2.last = "yuusaku"
 	fmt.Println(u2.first, u2.last)
 */
-	hys_structure_declaration ()
+	//hys_structure_declaration ()
+
+	//u := Hys{"hayashi", "yuusaku"}
+	//u.hys_method()
+	h := Hys{"hayashi", "yuusaku"}
+	h.hys_method()
+
+	//y := Ysk{"hayashi", "yuusaku"}
+	y := Ysk{}
+	y.hys_method()
 }
 
 func hys_structure_declaration () {
 	u := Hys{"hayashi", "yuusaku"}
 	fmt.Println(u.first, u.last)
 }
+
